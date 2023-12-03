e798c1bcc0e01ff02e9757ced281082a00d1d2c2
ksholovko <132094880+ksholovko@users.noreply.github.com>
2023-12-03 16:21:18 +0000

    Deploying to gh-pages from @ ksholovko/goit-react-hw-08-phonebook@b6fb576ba25d0c56434b8c1a56e71260db941538 🚀

---
+++ static/js/415.2ca45daf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{415:function(e,n,o){o.r(n);var t=o(135),r=o(184);n.default=function(){return(0,r.jsxs)("div",{style:{display:"flex",marginTop:"25vh",justifyContent:"center",alignItems:"center",gap:"0.5rem"},children:[(0,r.jsx)("h1",{style:{color:"black"},children:" Create Your Personal Phonebook "}),(0,r.jsx)(t.nl4,{fill:"#ff923cba",fontSize:"2em"})]})}}}]);
+//# sourceMappingURL=415.2ca45daf.chunk.js.map
